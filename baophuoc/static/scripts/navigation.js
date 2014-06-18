22aaeb1324dd76bc462495394356ebc4353e33dd
nise2710 <duytantran@hotmail.com>
2014-06-18 16:13:57 -0700

    update full site

---
+++ baophuoc/static/scripts/navigation.js
@@ -0,0 +1,2 @@
+/* base.js */
+/* Copyright 2014 by DTT - Bao Phuoc Temple */
--- baophuoc/static/scripts/thongtin/thongtin.js
+++
@@ -1,8 +0,0 @@
-/* dropdown sidebar script */
-$(document).ready(function () {
-	$('.dropdown').hover(
-		function () { $(this).find('.sub-menu').fadeIn(500);},
-		function () { $(this).find('.sub-menu').hide();}
-	);
-});
-/* end sidebar script */
